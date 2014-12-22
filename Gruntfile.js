cc89c14a8f79e79195ab3fce95a6cf6451ae59f4
Joseph Wynn <joseph@wildlyinaccurate.com>
2014-12-22 18:22:35 +0000

    Add grunt-uncss

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-uncss');
+
+    grunt.initConfig({
+        uncss: {
+            dist: {
+                options: {
+                    htmlroot: '_site'
+                },
+                files: {
+                    '_site/css/main.css': [
+                        '_site/index.html',
+                        '_site/about/index.html',
+                        '_site/projects/index.html',
+                        '_site/a-hackers-guide-to-git/index.html',
+                        '_site/transitioning-to-a-new-keyboard-layout/index.html',
+                        '_site/understanding-javascript-inheritance-and-the-prototype-chain/index.html',
+                        '_site/worst-fonts-for-programming/index.html'
+                    ]
+                }
+            }
+        }
+    });
+
+};
