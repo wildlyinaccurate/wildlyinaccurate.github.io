68cb102c444c8e4ab5ba3d9e61e58ac17eb92c82
Joseph Wynn <joseph@wildlyinaccurate.com>
2014-12-23 21:16:30 +0000

    Allow more tags in post summaries

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -7,7 +7,7 @@ module Jekyll
         preserve_elements = preserve_elements.split
       else
         # Default, sane elements
-        preserve_elements = %w[a b strong i em p br h2 h3 h4 h5 h6 pre code]
+        preserve_elements = %w[a b strong i em p br h2 h3 h4 h5 h6 pre code ul li]
       end
 
       Sanitize.fragment(html, :elements => preserve_elements, :remove_contents => true)
