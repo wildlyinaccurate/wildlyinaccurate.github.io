038cad5fde1a1986f2a8a63d54972a217efd49b2
Joseph Wynn <joseph@wildlyinaccurate.com>
2014-12-24 22:36:31 +0000

    Better post summary filtering
     - Allow lists, but remove Kramdown-generated ToC
     - Allow divs
     - Strip spans without removing the content
     - Allow href attribute on a elements

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -2,15 +2,43 @@ require 'sanitize'
 
 module Jekyll
   module SanitizeFilter
-    def sanitize(html, preserve_elements = nil)
-      if preserve_elements
-        preserve_elements = preserve_elements.split
-      else
-        # Default, sane elements
-        preserve_elements = %w[a b strong i em p br h2 h3 h4 h5 h6 pre code]
+    def sanitize(html)
+      preserve_elements = %w[
+        a
+        b strong i em
+        p br
+        h2 h3 h4 h5 h6
+        pre code
+        span div
+        ul ol li
+      ]
+
+      # Remove any Kramdown table of contents
+      toc_remover = lambda do |env|
+        return unless env[:node_name] == 'ul'
+
+        node = env[:node]
+
+        if node['id'] == 'markdown-toc'
+          node.unlink
+        end
       end
 
-      Sanitize.fragment(html, :elements => preserve_elements, :remove_contents => true)
+      # Remove spans but keep their content
+      span_transformer = lambda do |env|
+        return unless env[:node_name] == 'span'
+
+        env[:node].replace(env[:node].content)
+      end
+
+      Sanitize.fragment(html,
+        :remove_contents => true,
+        :elements        => preserve_elements,
+        :transformers    => [toc_remover, span_transformer],
+        :attributes      => {
+          'a' => ['href'],
+        }
+      )
     end
   end
 end
