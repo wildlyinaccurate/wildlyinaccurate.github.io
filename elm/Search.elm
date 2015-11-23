70039d9770d945b5f9774e31500bb7c68f01072e
Joseph Wynn <joseph@wildlyinaccurate.com>
2015-11-23 22:37:31 +0000

    I think this is the right approach

--- elm/Search.elm
+++ elm/Search.elm
@@ -1,20 +1,36 @@
 module Search where
 
-import String exposing (concat)
+import List exposing (filterMap, map)
+import Regex exposing (caseInsensitive, regex)
+import String exposing (concat, trim)
 
+import Effects exposing (Effects, Never)
 import Html exposing (..)
-import Html.Attributes exposing (class, placeholder, value)
-import Html.Events exposing (onKeyUp)
+import Html.Attributes exposing (class, href, placeholder, value)
+import Html.Events exposing (on, targetValue)
+import Json.Decode as Json
 import Signal exposing (Signal, Address)
-import StartApp.Simple exposing (start)
+import StartApp exposing (start)
+
+
+app =
+  start
+    { init = init
+    , update = update
+    , view = view
+    , inputs = []
+    }
 
 
 main =
-  start
-    { model = model getSearchData
-    , update = update
-    , view = view
-    }
+    app.html
+
+
+init : (Model, Effects Action)
+init =
+    ( model getSearchData
+    , Effects.none
+    )
 
 
 port getSearchData : Maybe SearchData
@@ -24,15 +40,14 @@ port getSearchData : Maybe SearchData
 
 type alias Model =
     { query : String
-    , results : List Result
+    , results : List SearchResult
     , data : SearchData
     }
 
 
-type alias Result =
+type alias SearchResult =
     { title : String
     , url : String
-    , type' : String
     }
 
 
@@ -66,22 +81,70 @@ emptyModel =
     Model "" [] (SearchData [] [] [])
 
 
-getResults : Model -> List Result
+getResults : Model -> List SearchResult
 getResults model =
-    []
+    let
+        fp = filterMap (postToResult model.query)
+        ft = filterMap (stringToResult model.query "tag")
+        fc = filterMap (stringToResult model.query "category")
+        md = model.data
+    in
+        List.concat
+            [ fp md.posts
+            , ft md.tags
+            , fc md.categories
+            ]
+
+
+stringToResult : String -> String -> String -> Maybe SearchResult
+stringToResult query type' item =
+    if matchString query item then
+        Just <| SearchResult (concat [type', ": ", item]) (concat ["/", type', "/", item, "/"])
+
+    else
+        Nothing
+
+
+postToResult : String -> Post -> Maybe SearchResult
+postToResult query post =
+    if (matchString query post.title) then
+        Just <| SearchResult post.title post.url
+
+    else
+        Nothing
+
+
+matchString : String -> String -> Bool
+matchString needle haystack =
+    Regex.contains (caseInsensitive (regex needle)) haystack
 
 
 ---- UPDATE ----
 
 type Action
-    = Search String
+    = UpdateQuery String
+    | UpdateResults
 
 
-update : Action -> Model -> Model
+update : Action -> Model -> (Model, Effects Action)
 update action model =
     case action of
-        Search query ->
-            { model | query = query }
+        UpdateQuery query ->
+            ( { model | query = query }
+            --, Effects.task UpdateResults
+            , Effects.none
+            )
+
+        UpdateResults ->
+            if String.length (trim model.query) == 0 then
+                ( model
+                , Effects.none
+                )
+
+            else
+                ( { model | results = getResults model }
+                , Effects.none
+                )
 
 
 ---- VIEW ----
@@ -91,7 +154,8 @@ view address model =
     div
         []
         [ searchInput address model
-        , searchResults model ]
+        , searchResults model
+        ]
 
 
 searchInput address model =
@@ -99,7 +163,8 @@ searchInput address model =
         [ class "search-input"
         , value model.query
         , placeholder "Enter query to search"
-        , onKeyUp address (\_ -> Search) ]
+        , on "input" targetValue (Signal.message address << UpdateQuery)
+        ]
         []
 
 
@@ -109,32 +174,38 @@ searchResults model =
         [ resultsTitle model
         , ul
             [ class "search-results__results" ]
-            (List.map searchResult model.results)
+            (map searchResult model.results)
         ]
-
-
-resultsTitle model =
-    let
-        resultsCount = List.length model.results
-    in
-        if resultsCount == 0 then
-            if String.length model.query > 0 then
-                h2
-                    [ class "search-results__title" ]
-                    [ text
-                        <| concat ["No results found for '", model.query, "'"]
-                    ]
-            else
-                span [] []
-        else
-            h2
-                [ class "search-results__title" ]
-                [ text
-                    <| concat [(toString resultsCount), " results for '", model.query, "'"]
-                ]
 
 
 searchResult result =
     li
         [ class "result-item" ]
-        []
+        [ h4
+            [ class "result-item__title" ]
+            [ a
+                [ class "result-item__link"
+                , href result.url
+                ]
+                [ text result.title ]
+            ]
+        ]
+
+
+resultsTitle model =
+    if List.length model.results == 0 then
+        if String.length (trim model.query) > 0 then
+            h2
+                [ class "search-results__title" ]
+                [ text
+                    <| concat [ "No results found for '", model.query, "'" ]
+                ]
+        else
+            span [] []
+    else
+        h2
+            [ class "search-results__title" ]
+            [ text
+                <| concat [ "Search results for '", model.query, "'" ]
+            ]
+
