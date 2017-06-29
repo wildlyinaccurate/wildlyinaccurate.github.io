002c49af6a9974b275146c8a959843d665df6ca9
Joseph Wynn <joseph@wildlyinaccurate.com>
2017-06-29 22:21:23 +0100

    Fresh branch with current build at 656c333f63a39793866f9fb3caf57fc3fe50dac1

---
+++ js/collection-bootstrap.705a83f.js
@@ -0,0 +1,32 @@
+import { Elm } from './compiled/Collection'
+
+var mountPoint = document.createElement('div')
+var staticContainer = document.querySelector('.collection-container')
+
+Elm.Main.init({
+    node: mountPoint,
+    flags: getData()
+})
+
+staticContainer.innerHTML = ''
+staticContainer.parentNode.appendChild(mountPoint)
+
+function getData() {
+    var container = document.querySelector('article')
+    var items = container.querySelectorAll('[data-collection-item]')
+
+    var data = [].slice.call(items).map(function(item) {
+        return {
+            title: item.querySelector('[data-title]').textContent,
+            url: item.querySelector('[data-url]').getAttribute('href'),
+            description: item.querySelector('[data-description] > p').innerHTML,
+            category: item.getAttribute('data-category'),
+            tags: JSON.parse(item.getAttribute('data-tags'))
+        }
+    })
+
+    return {
+        items: data,
+        filters: []
+    }
+}
---
+++ js/search.c4c98a7.js
@@ -0,0 +1,114 @@
+var Hogan = require('hogan.js')
+
+function Search(data) {
+    this.data = data
+    this.cache = {}
+}
+
+Search.prototype.matchArray = function(array, needle) {
+    var _this = this
+
+    return array.some(function(val) {
+        return _this.match(val, needle)
+    })
+}
+
+Search.prototype.match = function(str, needle) {
+    return str.match(new RegExp(needle, 'i'))
+}
+
+Search.prototype.query = function(query) {
+    var _this = this
+
+    query = query.toLocaleLowerCase()
+
+    if (!this.cache[query]) {
+        var results = []
+
+        this.data.posts.forEach(function(post) {
+            if (
+                _this.match(post.title, query) ||
+                _this.matchArray(post.categories, query) ||
+                _this.matchArray(post.tags, query)
+            ) {
+                results.push(post)
+            }
+        })
+
+        this.data.tags.forEach(function(tag) {
+            if (_this.match(tag, query)) {
+                results.push({
+                    type: 'tag',
+                    title: tag,
+                    url: '/tag/' + tag + '/'
+                })
+            }
+        })
+
+        this.data.categories.forEach(function(category) {
+            if (_this.match(category, query)) {
+                results.push({
+                    type: 'category',
+                    title: category,
+                    url: '/category/' + category + '/'
+                })
+            }
+        })
+
+        this.cache[query] = results
+    }
+
+    return this.cache[query]
+}
+
+var input = document.getElementById('query')
+var resultsContainer = document.getElementById('results')
+var search
+var debounce
+
+var resultsTemplate = Hogan.compile(
+    document.getElementById('search-tmpl').innerHTML
+)
+
+// Populate the input from the 'query' parameter
+var matches = window.location.search.match(/query=([^&]+)/)
+
+if (matches) {
+    input.value = matches[1]
+}
+
+var xhr = new XMLHttpRequest()
+xhr.onload = onSearchDataLoaded
+xhr.open('get', '/search.json', true)
+xhr.send()
+
+function onSearchDataLoaded() {
+    search = new Search(JSON.parse(this.responseText))
+
+    // Perform an initial search in case the user was typing while the data
+    // was being loaded
+    performSearch(input)
+
+    input.addEventListener('keyup', performSearch.bind(this, input))
+}
+
+function performSearch(input) {
+    clearTimeout(debounce)
+
+    debounce = setTimeout(function() {
+        var query = input.value
+
+        if (query.length > 2) {
+            renderResults(query, search.query(query))
+        } else {
+            renderResults()
+        }
+    }, 150)
+}
+
+function renderResults(query, results) {
+    resultsContainer.innerHTML = resultsTemplate.render({
+        query: query,
+        results: results,
+    })
+}
