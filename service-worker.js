e8e6ad3fa8cf00cdbaad6f44d6e8d28d2c93e199
Joseph Wynn <joseph@wildlyinaccurate.com>
2017-08-06 17:02:20 +0100

    Switch to network-first SW strategy

--- service-worker.js
+++ service-worker.js
@@ -5,24 +5,18 @@ self.addEventListener('fetch', event => {
   const requestUrl = new URL(event.request.url)
 
   if (requestUrl.origin === baseUrl.origin) {
-    event.respondWith(staleWhileRevalidate(event.request))
+    event.respondWith(networkFirst(event.request))
   }
 })
 
-function staleWhileRevalidate (request) {
-  return caches.open(RESOURCE_CACHE).then(cache =>
-    cache.match(request).then(response => {
-      const fetchPromise = fetch(request).then(networkResponse => {
-        cache.put(request, networkResponse.clone())
-
-        console.debug('Cache updated for', request.url)
-
-        return networkResponse
-      }).catch(() => {
-        console.debug('Using stale cache for', request.url)
+function networkFirst (request) {
+  return fetch(request)
+    .then(response => {
+      caches.open(RESOURCE_CACHE).then(cache => {
+        cache.put(request, response)
       })
 
-      return response || fetchPromise
+      return response.clone()
     })
-  )
+    .catch(() => caches.match(request))
 }
