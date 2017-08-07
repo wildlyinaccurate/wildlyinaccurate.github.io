9005f4fedd3810460257be32c68ae67265cfa4fe
Joseph Wynn <joseph@wildlyinaccurate.com>
2017-08-07 22:47:34 +0100

    Use staleWhileRevalidate SW strategy for images

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,11 @@ self.addEventListener('fetch', event => {
   const requestUrl = new URL(event.request.url)
 
   if (requestUrl.origin === baseUrl.origin) {
-    event.respondWith(networkFirst(event.request))
+    if (requestUrl.pathname.substr(0, 8) === '/assets/') {
+      event.respondWith(staleWhileRevalidate(event.request))
+    } else {
+      event.respondWith(networkFirst(event.request))
+    }
   }
 })
 
@@ -19,4 +23,22 @@ function networkFirst (request) {
       return response.clone()
     })
     .catch(() => caches.match(request))
+}
+
+function staleWhileRevalidate (request) {
+  return caches.open(RESOURCE_CACHE).then(cache =>
+    cache.match(request).then(response => {
+      const fetchPromise = fetch(request).then(networkResponse => {
+        cache.put(request, networkResponse.clone())
+
+        console.debug('Cache updated for', request.url)
+
+        return networkResponse
+      }).catch(() => {
+        console.debug('Using stale cache for', request.url)
+      })
+
+      return response || fetchPromise
+    })
+  )
 }
